93e1383a60a257a22b36106ace9213a1c80b88bd
fr <fr@integrity.pt>
2017-06-09 00:21:02 +0100

    Adding a few more tests

--- playground/playground.php
+++ playground/playground.php
@@ -1,5 +1,6 @@
 <?php
 $i = $_GET['injection'];
+
 switch($_GET['test']){
 	case 1:
 		print $i;
@@ -11,9 +12,16 @@ switch($_GET['test']){
 		print "<tag attribute='$i'></tag>";
 		break;
 	case 4:
-		print "<tag attribute=$i></tag>"
+		print "<tag attribute=$i></tag>";
 		break;
-	
+    case 5:
+        print "<script>$i</script>";
+        break;
+    case 6:
+        print "<script>var i=\"http://localhost/$i\"</script>";
+        break;
+	default:
+        print "No test was selected";
 
 }
 ?>
