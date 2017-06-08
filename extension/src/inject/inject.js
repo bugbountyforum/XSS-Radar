7badb7d257f87872a27860b4a0477330fe18e871
Olivier Beg <olivier@hotmail.lv>
2017-06-08 23:22:21 +0200

    tabs to spaces, Atom, we have to talk..

--- extension/src/inject/inject.js
+++ extension/src/inject/inject.js
@@ -2,32 +2,32 @@
 
 class Xsshamster {
 
-	constructor () {
-		this.listener()
-	}
+  constructor () {
+    this.listener()
+  }
 
-	listener () {
-		chrome.runtime.onMessage.addListener((instruction, sender, sendResponse) => {
-			console.log(instruction)
-			if (this.validateInstruction(instruction)) {
-				this.execute(instruction)
-			} else {
-				console.error('Invalid instruction', instruction)
-			}
-		})
-	}
+  listener () {
+    chrome.runtime.onMessage.addListener((instruction, sender, sendResponse) => {
+      console.log(instruction)
+      if (this.validateInstruction(instruction)) {
+        this.execute(instruction)
+      } else {
+        console.error('Invalid instruction', instruction)
+      }
+    })
+  }
 
-	execute (instruction) {
-		this[instruction.function]()
-	}
+  execute (instruction) {
+    this[instruction.function]()
+  }
 
-	validateInstruction (instruction) {
-		return typeof instruction == 'object' && typeof instruction.function != 'undefined'
-	}
-
-	scan () {
-		alert('Scan triggered.')
-	}
+  validateInstruction (instruction) {
+    return typeof instruction == 'object' && typeof instruction.function != 'undefined'
+  }
+  
+  scan () {
+    alert('Scan triggered.')
+  }
 }
 
 new Xsshamster
