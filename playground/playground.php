267fb7032f401ddab3c577ccda50e0fbd2749af0
fr <fr@integrity.pt>
2017-06-09 00:31:21 +0100

    tabs,spaces,stuff

--- playground/playground.php
+++ playground/playground.php
@@ -2,25 +2,25 @@
 $i = $_GET['injection'];
 
 switch($_GET['test']){
-	case 1:
-		print $i;
-		break;
-	case 2:
-		print "<tag attribute=\"$i\"></tag>";
-		break;
-	case 3:
-		print "<tag attribute='$i'></tag>";
-		break;
-	case 4:
-		print "<tag attribute=$i></tag>";
-		break;
+    case 1:
+        print $i;
+        break;
+    case 2:
+        print "<tag attribute=\"$i\"></tag>";
+        break;
+    case 3:
+        print "<tag attribute='$i'></tag>";
+        break;
+    case 4:
+        print "<tag attribute=$i></tag>";
+        break;
     case 5:
         print "<script>$i</script>";
         break;
     case 6:
         print "<script>var i=\"http://localhost/$i\"</script>";
         break;
-	default:
+    default:
         print "No test was selected";
 
 }
