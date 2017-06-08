6d9c2dc40257d00233c0998ac7ff3ebf35cb7df5
Ibram Marzouk <0xibram@users.noreply.github.com>
2017-06-08 19:31:17 -0400

    more tests

--- playground/playground.php
+++ playground/playground.php
@@ -11,7 +11,32 @@ switch($_GET['test']){
 		print "<tag attribute='$i'></tag>";
 		break;
 	case 4:
-		print "<tag attribute=$i></tag>"
+		print "<tag attribute=$i></tag>";
+		break;
+	case 5:
+		$i = str_replace("<", "", $i);
+		$i = str_replace(">", "", $i);
+		print "<tag attribute=\"$i\"></tag>";
+		break;
+	case 6:
+		$i = str_replace("<", "", $i);
+		$i = str_replace(">", "", $i);
+		print "<tag attribute='$i'></tag>";
+		break;
+	case 7:
+		print "<script>\n";
+		print $i
+		print "\n</script>";
+		break;
+	case 8:
+		print "<script>\n";
+		print "var i = \"$i\"";
+		print "\n</script>";
+		break;
+	case 9:
+		print "<script>\n";
+		print "var i = '".$i."'";
+		print "\n</script>";
 		break;
 	
 
