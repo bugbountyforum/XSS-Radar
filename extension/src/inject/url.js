aec4c7309e4bf43f9783f03112db0691a0a2375f
Jack Cable <jackhcable@gmail.com>
2017-06-09 19:14:05 -0500

    Fix bug with url handling

--- extension/src/inject/url.js
+++ extension/src/inject/url.js
@@ -1,37 +1,43 @@
 class Url {
   
   get (url, param, payload) {
-    var search = this.search()
-    if (search != undefined) {
-      var found = false
-      var params = this.parameters(search)
-      params.forEach((p) => {
-        if (p == param) {
-          arams[p] = payload
-          found = true
-        }
-      })
-      if (!found) {
-        params[param] = payload
+    return this.appendStringToParam(url, param, payload)
+  }
+
+  // returns an array of query parameters
+  getQueryParams(url) {
+      var request = {};
+      var pairs = url.substring(url.indexOf('?') + 1).split('&');
+      for (var i = 0; i < pairs.length; i++) {
+          if (!pairs[i])
+              continue;
+          var pair = pairs[i].split('=');
+          request[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+       }
+       return request;
+  }
+
+  // Identify parameters of query
+  // TODO: look in url as well
+  appendStringToParam(url, param, string) {
+    console.log(param)
+    var originalParams = this.getQueryParams(url);
+    var updatedParams = {};
+    for (var key in originalParams) {
+      if (Object.prototype.hasOwnProperty.call(originalParams, key)) {
+          if (key == param) {
+            updatedParams[key] = originalParams[key] + string;
+          } else {
+            updatedParams[key] = originalParams[key];
+          }
       }
-      return url.split('?')[0] + '?' + params.join('&')
-    } else {
-      return url.split('?')[0] + '?' + param + '=' + decodeURIComponent(payload)
     }
+    return this.updateUrl(url, updatedParams);
   }
 
-  search (url) {
-    let link = document.createElement(url)
-    link.href = url
-    return link.search
+  // returns the url with the specified params
+  updateUrl(url, params) {
+    return url.split('?')[0] + '?' + Object.keys(params).map(k => k + '=' + encodeURIComponent(params[k])).join('&');
   }
 
-  parameters (query) {
-    var result = {}
-    query.split('&').forEach(function(part) {
-      var item = part.split('=')
-      result[item[0]] = decodeURIComponent(item[1])
-    });
-    return result;
-  }
 }
