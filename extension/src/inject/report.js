6ed3be54a7e43be68e7cfe7ef75729ea0bb8dc97
= <dgisser@gmail.com>
2017-10-18 19:50:03 -0400

    added message if there are no vulnerabilities found

--- extension/src/inject/report.js
+++ extension/src/inject/report.js
@@ -9,8 +9,13 @@ class Report {
     let title = document.createElement('h1')
     title.setAttribute('style', 'width: 100%;font: 30px verdana;padding: 0 0.5em;')
     title.innerHTML = 'Findings'
+    let noObjects = document.createElement('p')
+    noObjects.setAttribute("id", "noObjects")
+    noObjects.setAttribute('style', 'width:100%;padding:1em;border:0;outline:0;border-bottom:1px #eee solid;font-size: 16px;')
+    noObjects.innerHTML = 'XSS-Radar did not detect any cross-site scripting vulnerabilities.'
     let body = this.w.document.body
     body.appendChild(title)
+    body.appendChild(noObjects)
   }
 
   add (report) {
@@ -18,6 +23,11 @@ class Report {
     element.setAttribute('value', report)
     element.setAttribute('style', 'width:100%;padding:1em;border:0;outline:0;border-bottom:1px #eee solid;font-size: 16px;')
     let body = this.w.document.body
+    // we have found vulnerabilities, so remove no vulnerabilities indicator
+    var noObjects = body.querySelector("#noObjects")
+    if (noObjects !== null) {
+      body.removeChild(noObjects);
+    }
     body.appendChild(element)
   }
 }
